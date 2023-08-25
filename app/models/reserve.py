17847c6ae751959cd108814fbc075e8d51dffb00
AugustoCastroo <au.castro@alumno.um.edu.ar>
2023-08-25 18:07:42 -0300

    modificando reserce.py

--- app/models/reserve.py
+++ app/models/reserve.py
@@ -6,10 +6,34 @@ from datetime import datetime
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))
+    _name = db.Column(db.String(120), unique=True)
+    _email = db.Column(db.String(120), unique=True)
+    _password = db.Column(db.String(120))
     reservations = relationship('Reservation', back_populates='user')
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def email(self):
+        return self._email
+    
+    @email.setter
+    def email(self, email):
+        self._email = email
+
+    @property
+    def password(self):
+        return self._password
+    
+    @password.setter
+    def password(self, password):
+        self._password = password
 
     def __repr__(self):
         return f'User: [ID: {self.id}, Name: {self.name}, Email: {self.email}, Password: {self.password}]'
@@ -17,8 +41,16 @@ class User(db.Model):
 class Schedule(db.Model):
     __tablename__ = 'schedule'
     id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.String(10), nullable=False, unique=True)
+    _time = db.Column(db.String(10), nullable=False, unique=True)
     reservations = relationship('Reservation', back_populates='schedule')
+
+    @property
+    def time(self):
+        return self._time
+    
+    @time.setter
+    def time(self, time):
+        self._time = time
 
     def __repr__(self):
         return f'Schedule: [ID: {self.id}, Time: {self.time}]'
