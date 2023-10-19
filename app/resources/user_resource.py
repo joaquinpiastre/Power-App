1b213f6d130c8aaea4d5a12dc820733114870a38
santipaez <sr.paez@alumno.um.edu.ar>
2023-10-19 18:47:52 -0300

    Cambios en user_service y nuevos archivos añadidos al .gitignore

--- app/resources/user_resource.py
+++ app/resources/user_resource.py
@@ -1,5 +1,9 @@
 from flask import jsonify, Blueprint
 from app.services.user_service import UserService
+from app.mapping.response_schema import ResponseSchema
+from app.mapping.user_schema import UserSchema
+from app.models.responseMessage import ResponseBuilder
+
 
 user = Blueprint('user', __name__)
 
@@ -8,3 +12,16 @@ def index():
     service = UserService()
     list = service.find_all()
     return jsonify(list, "W"), 200
+
+"""
+id: int ingresado por el usuario
+return: json con los datos del usuario
+"""
+@user.route('/user/<int:id>', methods=['GET'])
+def find(id):
+    service = UserService()
+    response_builder = ResponseBuilder()
+    response_builder.add_message("Usuario encontrado")\
+        .add_status_code(200)\
+        .add_data(UserSchema().dump(service.find(id)))
+    return ResponseSchema().dump(response_builder.build()), 200
---
+++ app/services/email_service.py
@@ -0,0 +1,8 @@
+class EmailService():
+    def __init__():
+        #TODO: Configuracion de servidor de correo
+        #self.smtp_server = smtplib.SMTP('mailhost', 25)
+        pass
+    
+    def send_email(self, email, message):
+        print(f"{message} for email: {email}")
